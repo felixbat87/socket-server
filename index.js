47deb498ca487fb9f17291afeacc6ccceb26bd0b
FelixOtero-museodelcanal29 <fotero@museodelcanal.com>
2023-06-08 17:05:51 -0500

    Actualizacin de Emision

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const router = require("./routes/router");
 const bodyParser = require('body-parser');
+//Socket.io
+const http = require('http');
+const { Server } = require("socket.io");
+
+////////////////////////////////////////////
+
 const app = express();
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //Body Parse
@@ -11,10 +19,48 @@ app.use(bodyParser.json());
 //cors
 app.use(cors({origin:true,credentials:true}));
 //rutas
-app.use('/api', router);
-
+app.use('/', router);
 ///////////////////////////////////////////////////
+//creando server de soket.io
+const server = http.createServer(app);
+const io = new Server(server,{ cors: { origin: true, credentials: true } });
+
+
 const PORT = process.env.PORT||5000
-app.listen(PORT, () => {
+//express normal
+// app.listen(PORT, () => {
+//     console.log("Servidor en ejecucion:" + PORT);
+// });
+server.listen(PORT, () => {
     console.log("Servidor en ejecucion:" + PORT);
+  });
+//io sockets functions
+
+console.log('escuchando conexiones - sockets');
+
+io.on('connection', (cliente) => { 
+
+  console.log('Cliente Conectado');
+  
+  //mensajes
+  mensaje(cliente);
+  //Desconectar
+  desconectar(cliente);
+
 });
+//funcion desconectar
+
+function desconectar(cliente){
+  cliente.on('disconnect',()=>{
+    console.log("Cliente Desconectado");
+  });
+}
+//Escuchar mensajes
+function mensaje(cliente){
+  cliente.on('mensaje',(payload={de:String,cuerpo:String})=>{
+   console.log("Mensaje Recibido", payload);
+  });
+}
+
+
+
