7beba769114bcb174312b4a6c54969736c56e5e7
FelixOtero-museodelcanal29 <fotero@museodelcanal.com>
2023-06-09 12:52:00 -0500

    Se anexo emision mensaje nuevo

--- index.js
+++ index.js
@@ -5,12 +5,8 @@ const bodyParser = require('body-parser');
 //Socket.io
 const http = require('http');
 const { Server } = require("socket.io");
-
 ////////////////////////////////////////////
-
 const app = express();
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //Body Parse
@@ -24,43 +20,39 @@ app.use('/', router);
 //creando server de soket.io
 const server = http.createServer(app);
 const io = new Server(server,{ cors: { origin: true, credentials: true } });
-
-
 const PORT = process.env.PORT||5000
-//express normal
+// express normal
 // app.listen(PORT, () => {
-//     console.log("Servidor en ejecucion:" + PORT);
+// console.log("Servidor en ejecucion:" + PORT);
 // });
 server.listen(PORT, () => {
     console.log("Servidor en ejecucion:" + PORT);
   });
 //io sockets functions
-
 console.log('escuchando conexiones - sockets');
-
 io.on('connection', (cliente) => { 
-
   console.log('Cliente Conectado');
-  
   //mensajes
   mensaje(cliente);
   //Desconectar
   desconectar(cliente);
-
 });
 //funcion desconectar
-
 function desconectar(cliente){
   cliente.on('disconnect',()=>{
-    console.log("Cliente Desconectado");
+  console.log("Cliente Desconectado");
   });
 }
 //Escuchar mensajes
 function mensaje(cliente){
+
   cliente.on('mensaje',(payload={de:String,cuerpo:String})=>{
    console.log("Mensaje Recibido", payload);
+   io.emit('mensaje-nuevo',payload);
   });
+  
 }
+
 
 
 
